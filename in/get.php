faa28388c61d953ce34ac1d13338589089faf383
C.J.Gutteridge <totl@srv02312.soton.ac.uk>
2018-09-08 16:43:48 +0100

    MVP

--- in/get.php
+++
@@ -1,3 +0,0 @@
-<?php
-file_put_contents( "leaflet.ajax.js", file_get_contents( "https://raw.githubusercontent.com/calvinmetcalf/leaflet-ajax/master/dist/leaflet.ajax.min.js" ));
-
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+$sets = preg_split( "/,/", preg_replace( "/[^A-Za-z0-9,]/","",$_GET['sets']));
 
 ?>
 <!DOCTYPE html>
@@ -20,6 +20,17 @@ html, body { margin:0; }
         position: absolute;
         width: 100%;
         height: 100%;
+}
+.popup-title {
+	font-size: 120%;
+	font-weight: bold;
+}
+.popup-table th {
+	vertical-align:top;
+	text-align: right
+}
+.picMarker {
+	border: solid 1px black;
 }
   </style>
 </head>
@@ -42,29 +53,69 @@ var geojsonMarkerOptions = {
     opacity: 1,
     fillOpacity: 0.8
 };
-
-var geojsonLayer = new L.GeoJSON.AJAX("geojson.php?id=Plaques",{
+var options = {
     pointToLayer: function (feature, latlng) {
-	var myIcon = L.icon({
-    		iconUrl: feature["properties"]["depiction"],
-    		iconSize: [100, 66],
-    		iconAnchor: [50, 33],
-    		popupAnchor: [-3, -76],
-    		shadowSize: [68, 95],
-    		shadowAnchor: [22, 94]
-	});
-        return L.marker(latlng, {icon: myIcon} );
+        var url = "";
+        if( feature["properties"] && feature["properties"]["depiction"] ) {
+		url = feature["properties"]["depiction"];
+		var myIcon = L.icon({
+    			iconUrl: url,
+    			iconSize: [100, 66],
+    			iconAnchor: [50, 33],
+    			popupAnchor: [0, -33],
+    			shadowSize: [68, 95],
+    			shadowAnchor: [22, 94],
+			className: "picMarker"
+		});
+        	return L.marker(latlng, {icon: myIcon} );
+	} else {
+	        return L.circleMarker(latlng, geojsonMarkerOptions);
+	}
+    },
+    onEachFeature: function(feature, layer) {
+	var popupHtml = "";
+	popupHtml += "<div class='popup-title'>"+feature.properties.label+"</div>";
+	popupHtml += "<table class='popup-table'>";
+	if( feature.properties['depiction'] ) {
+		popupHtml += "<img src='"+feature.properties.depiction+"' style='width:100%' />";
+	}
+	var terms = Object.keys( feature.properties ).sort();
+	//alert( JSON.stringify( terms ));	
+	for( var i=0;i<terms.length;++i ) {
+		var term = terms[i];
+		var value = feature.properties[term];
+		term = term.trim();
+		if( value != "" ) {
+			if( term == "copyrightURL" || term == "homepage" || term == "depiction" || term == "wikipediaPage" ) {
+				value = "<a href='"+value+"'>"+value+"</a>";
+			}
+			popupHtml+="<tr><th>"+term+":</th><td>"+value+"</td></tr>";
+		}
+	}
+	popupHtml += "</table>";
+	if( feature.properties['copyrightText'] ) {
+		var copyright = feature.properties['copyrightText'];
+		if( feature.properties['copyrightURL'] ) {
+			copyright = "<a href='"+feature.properties['copyrightURL']+"'>"+copyright+"</a>";
+		}
+		popupHtml+="<p>&copy; "+copyright+"</p>";
+	}
+       	layer.bindPopup(popupHtml,  { maxWidth : 500 });
     }
-
-});       
-geojsonLayer.addTo(map);
+};
+//new L.GeoJSON.AJAX("geojson.php?id=Plaques",options ).addTo(map);
+//new L.GeoJSON.AJAX("geojson.php?id=Benches",options ).addTo(map);
+//new L.GeoJSON.AJAX("geojson.php?id=Crime",options ).addTo(map);
 
 map.fitBounds([
 	[51.10719,-1.39286],
 	[51.03772,-1.26411]
 ]);
-
-
+<?php
+foreach( $sets as $set ) {
+	print "new L.GeoJSON.AJAX(\"geojson.php?id=$set\",options ).addTo(map);\n";
+}
+?>
 </script>
 
 </body></html>
